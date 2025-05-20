700c151e48a67e1bc21773c7c8efca96178f4112
NilNull818 <diaosi.dev@gmail.com>
2025-05-20 16:16:12 +0800

    add raylib demo

---
+++ DoubleHundredthEightiethProj/src/main.c
@@ -0,0 +1,218 @@
+#include "libco.h"
+#include "raylib.h"
+#include "stdio.h"
+#include "string.h"
+
+#define SCREEN_WIDTH (990)
+#define SCREEN_HEIGHT (660)
+#define ASSET_PATH "../assets/"
+
+cothread_t main_cothread;
+cothread_t story_cothread;
+
+typedef enum CommandType
+{
+    NONE = 0,
+    ECHO,
+    OPTION,
+} CommandType;
+
+typedef struct Command
+{
+    CommandType type;
+
+    union
+    {
+        struct
+        {
+            // In
+            const char* text;
+            int text_len;
+
+            // Working
+            float timer;
+            int text_i;
+        } echo;
+
+        struct
+        {
+            // In
+            const char* text;
+            int text_len;
+
+            const char* option1;
+            int option1_len;
+
+            const char* option2;
+            int option2_len;
+
+            // Working
+            float timer;
+            int text_i;
+            int space1;
+            int space2;
+            bool text_anim_done;
+
+            // Output
+            bool result;
+        } option;
+    };
+} Command;
+
+Command cmd;
+
+void echo(const char* text)
+{
+    // Set some data to tell the main thread what to do
+    cmd = (Command) {
+        .type = ECHO,
+        .echo.text = text,
+        .echo.text_len = strlen(text)
+    };
+
+    // Switch to main thread
+    co_switch(main_cothread);
+}
+
+bool option(const char* text, const char* option1, const char* option2)
+{
+    cmd = (Command) {
+        .type = OPTION,
+        .option.text = text,
+        .option.text_len = strlen(text),
+
+        .option.option1 = option1,
+        .option.option1_len = strlen(option1),
+
+        .option.option2 = option2,
+        .option.option2_len = strlen(option2),
+    };
+
+    co_switch(main_cothread);
+
+    return cmd.option.result;
+}
+
+void pause()
+{
+    cmd = (Command) {0};
+    co_switch(main_cothread);
+}
+
+void story(void)
+{
+    echo("Hi, my name is Sussie from the Southern Beans Tribe and I am the high lord of UwU.\n\n");
+    echo("I have developed the UwU movement during the Second Bread War in order to mobilize and win all the BROD.\n\n");
+
+    if (option("Will you join me in the cause of UwU or will you disgrace me by taking the path of -_- ?\n", "YES", "NO"))
+    {
+        echo("Amazin'");
+        pause();
+    }
+    else
+    {
+        echo("SHAME!");
+        pause();
+    }
+}
+
+int main(void)
+{
+    main_cothread = co_active();
+    story_cothread = co_create(1024 * 8, story, NULL);
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Awataw: The last Bwead bendew");
+    SetTargetFPS(60);
+
+    bool command_executed = true;
+
+    char text[1024 * 8] = {0};
+    int text_i = 0;
+
+    while (!WindowShouldClose())
+    {
+        if (command_executed)
+        {
+            co_switch(story_cothread);
+            command_executed = false;
+        }
+
+        switch (cmd.type)
+        {
+            case ECHO:
+            {
+                cmd.echo.timer += GetFrameTime();
+                if (cmd.echo.timer >= 0.01)
+                {
+                    cmd.echo.timer = 0;
+                    text[text_i++] = cmd.echo.text[cmd.echo.text_i++];
+                    if (cmd.echo.text_i == cmd.echo.text_len)
+                        command_executed = true;
+                }
+            } break;
+
+            case OPTION:
+            {
+                cmd.option.text_anim_done = cmd.option.text_i == cmd.option.text_len;
+
+                cmd.option.timer += GetFrameTime();
+                if (cmd.option.timer >= 0.01 && !cmd.option.text_anim_done)
+                {
+                    cmd.option.timer = 0;
+                    text[text_i++] = cmd.option.text[cmd.option.text_i++];
+
+                    cmd.option.text_anim_done = cmd.option.text_i == cmd.option.text_len;
+
+                    if (cmd.option.text_anim_done)
+                    {
+                        cmd.option.result = true;
+                        const char* option_text = TextFormat("\n>%s   %s\n\n", cmd.option.option1, cmd.option.option2);
+                        cmd.option.space1 = text_i + 1;
+                        cmd.option.space2 = text_i + 1 + cmd.option.option1_len + 3;
+                        memcpy(&text[text_i], option_text, strlen(option_text) + 1);
+                        text_i += strlen(option_text);
+                    }
+                }
+
+                if (cmd.option.text_anim_done)
+                {
+                    if (IsKeyPressed(KEY_LEFT))
+                    {
+                        cmd.option.result = true;
+                        text[cmd.option.space1] = '>';
+                        text[cmd.option.space2] = ' ';
+                    }
+
+                    if (IsKeyPressed(KEY_RIGHT))
+                    {
+                        cmd.option.result = false;
+                        text[cmd.option.space1] = ' ';
+                        text[cmd.option.space2] = '>';
+                    }
+
+                    if (IsKeyPressed(KEY_ENTER))
+                    {
+                        command_executed = true;
+                    }
+                }
+            } break;
+            default: break;
+        }
+
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        const int padd = 24;
+        //const Rectangle text_area = { padd, padd, GetScreenWidth() - padd * 2, GetScreenHeight() - padd * 2 };
+        const Vector2 text_area = { padd, padd, GetScreenWidth() - padd * 2, GetScreenHeight() - padd * 2 };
+        // DrawTextRec(GetFontDefault(), text, text_area, 20, 2, true, RAYWHITE);
+        DrawTextEx(GetFontDefault(), text, text_area, 20, 2, RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
---
+++ DoubleHundredthEightyFirstProj/src/light_my_ritual.c
@@ -0,0 +1,290 @@
+/*******************************************************************************************
+*
+*   LIGHT MY RITUAL [GLOBAL GAME JAM 2016]
+*
+*   Preparing a ritual session is not that easy.
+*   You must light all the candles before the astral alignment finishes...
+*   but dark creatures move in the shadows to put out all your lights!
+*   Be fast! Be smart! Light my ritual!
+*
+*   This game has been created using raylib 1.6 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2016 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#include "screens.h"    // NOTE: Declares global (extern) variables and screens functions
+
+#include <stdlib.h>     // Required for:
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+//----------------------------------------------------------------------------------
+// Shared Variables Definition (global)
+//----------------------------------------------------------------------------------
+GameScreen currentScreen = LOGO;
+Font font = { 0 };
+Color *lightsMap = NULL;
+int lightsMapWidth = 0;
+int lightsMapHeight = 0;
+
+//----------------------------------------------------------------------------------
+// Module Variables Definition (local)
+//----------------------------------------------------------------------------------
+static const int screenWidth = 1280;
+static const int screenHeight = 720;
+
+// Required variables to manage screen transitions (fade-in, fade-out)
+static float transAlpha = 0;
+static bool onTransition = false;
+static bool transFadeOut = false;
+static int transFromScreen = -1;
+static int transToScreen = -1;
+
+static Music music;
+
+//----------------------------------------------------------------------------------
+// Module Functions Declaration (local)
+//----------------------------------------------------------------------------------
+static void ChangeToScreen(int screen);     // Change to screen, no transition effect
+
+static void TransitionToScreen(int screen); // Request transition to next screen
+static void UpdateTransition(void);         // Update transition effect
+static void DrawTransition(void);           // Draw transition effect (full-screen rectangle)
+
+static void UpdateDrawFrame(void);          // Update and draw one frame
+
+//----------------------------------------------------------------------------------
+// Main entry point
+//----------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization (Note windowTitle is unused on Android)
+    //---------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "LIGHT MY RITUAL! [GGJ16]");
+
+    // Global data loading (assets that must be available in all screens, i.e. fonts)
+    InitAudioDevice();
+
+    Image image = LoadImage("resources/lights_map.png");  // Load image in CPU memory (RAM)
+
+    lightsMap = LoadImageColors(image);         // Get image pixels data as an array of Color
+    lightsMapWidth = image.width;
+    lightsMapHeight = image.height;
+
+    UnloadImage(image);                         // Unload image from CPU memory (RAM)
+
+    font = LoadFont("resources/font_arcadian.png");
+    //doors = LoadTexture("resources/textures/doors.png");
+    //sndDoor = LoadSound("resources/audio/door.ogg");
+
+    music = LoadMusicStream("resources/audio/ambient.ogg");
+    PlayMusicStream(music);
+    SetMusicVolume(music, 1.0f);
+
+    // Setup and Init first screen
+    currentScreen = LOGO;
+    InitLogoScreen();
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        UpdateDrawFrame();
+    }
+#endif
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    switch (currentScreen)
+    {
+        case LOGO: UnloadLogoScreen(); break;
+        case TITLE: UnloadTitleScreen(); break;
+        case GAMEPLAY: UnloadGameplayScreen(); break;
+        default: break;
+    }
+
+    // Unload all global loaded data (i.e. fonts) here!
+    UnloadFont(font);
+    UnloadMusicStream(music);
+    UnloadImageColors(lightsMap);
+
+    CloseAudioDevice();
+
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
+//----------------------------------------------------------------------------------
+// Module Functions Definition (local)
+//----------------------------------------------------------------------------------
+// Change to screen, no transition effect
+static void ChangeToScreen(int screen)
+{
+    switch (currentScreen)
+    {
+        case LOGO: UnloadLogoScreen(); break;
+        case TITLE: UnloadTitleScreen(); break;
+        case GAMEPLAY: UnloadGameplayScreen(); break;
+        default: break;
+    }
+
+    switch (screen)
+    {
+        case LOGO: InitLogoScreen(); break;
+        case TITLE: InitTitleScreen(); break;
+        case GAMEPLAY: InitGameplayScreen(); break;
+        default: break;
+    }
+
+    currentScreen = screen;
+}
+
+// Request transition to next screen
+static void TransitionToScreen(int screen)
+{
+    onTransition = true;
+    transFromScreen = currentScreen;
+    transToScreen = screen;
+}
+
+// Update transition effect
+static void UpdateTransition(void)
+{
+    if (!transFadeOut)
+    {
+        transAlpha += 0.05f;
+
+        if (transAlpha >= 1.0)
+        {
+            transAlpha = 1.0;
+
+            switch (transFromScreen)
+            {
+                case LOGO: UnloadLogoScreen(); break;
+                case TITLE: UnloadTitleScreen(); break;
+                case GAMEPLAY: UnloadGameplayScreen(); break;
+                default: break;
+            }
+
+            switch (transToScreen)
+            {
+                case LOGO:
+                {
+                    InitLogoScreen();
+                    currentScreen = LOGO;
+                } break;
+                case TITLE:
+                {
+                    InitTitleScreen();
+                    currentScreen = TITLE;
+                } break;
+                case GAMEPLAY:
+                {
+                    InitGameplayScreen();
+                    currentScreen = GAMEPLAY;
+                } break;
+                default: break;
+            }
+
+            transFadeOut = true;
+        }
+    }
+    else  // Transition fade out logic
+    {
+        transAlpha -= 0.05f;
+
+        if (transAlpha <= 0)
+        {
+            transAlpha = 0;
+            transFadeOut = false;
+            onTransition = false;
+            transFromScreen = -1;
+            transToScreen = -1;
+        }
+    }
+}
+
+// Draw transition effect (full-screen rectangle)
+static void DrawTransition(void)
+{
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(BLACK, transAlpha));
+}
+
+// Update and draw game frame
+static void UpdateDrawFrame(void)
+{
+    // Update
+    //----------------------------------------------------------------------------------
+    if (!onTransition)
+    {
+        switch(currentScreen)
+        {
+            case LOGO:
+            {
+                UpdateLogoScreen();
+
+                if (FinishLogoScreen()) TransitionToScreen(TITLE);
+
+            } break;
+            case TITLE:
+            {
+                UpdateTitleScreen();
+
+                if (FinishTitleScreen() == 1)
+                {
+                    StopMusicStream(music);
+                    TransitionToScreen(GAMEPLAY);
+                }
+
+            } break;
+            case GAMEPLAY:
+            {
+                UpdateGameplayScreen();
+
+                if (FinishGameplayScreen() == 1) ChangeToScreen(LOGO);
+                else if (FinishGameplayScreen() == 2) TransitionToScreen(TITLE);
+
+            } break;
+            default: break;
+        }
+    }
+    else
+    {
+        // Update transition (fade-in, fade-out)
+        UpdateTransition();
+    }
+
+    if (currentScreen != GAMEPLAY) UpdateMusicStream(music);
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        switch(currentScreen)
+        {
+            case LOGO: DrawLogoScreen(); break;
+            case TITLE: DrawTitleScreen(); break;
+            case GAMEPLAY: DrawGameplayScreen(); break;
+            default: break;
+        }
+
+        if (onTransition) DrawTransition();
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+}
